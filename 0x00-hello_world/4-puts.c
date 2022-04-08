a5deb27f2d17a541b8cd35b8b63a00e25b3d7a35
KwachOjunga <ojungakwach@gmail.com>
2022-04-07 17:16:54 -0700

    error fix

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-/*The program print a line using puts*/
+/*main - The program print a line using puts*/
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
