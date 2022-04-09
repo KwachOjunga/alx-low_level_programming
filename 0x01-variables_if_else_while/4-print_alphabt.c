12c20221e20cbc75aca4a421fc224f6bd9a96878
KwachOjunga <ojungakwach@gmail.com>
2022-04-08 19:43:41 -0700

    Append semi-colons

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -7,15 +7,15 @@
  */
 int main (void)
 {
-	char ch = 'a'
+	char ch = 'a';
 
 	while (ch <= 'z')
 	{
-		if (ch != 'e' && ch != 'q')
+		if (ch != 'e' && ch != 'q');
 		{
-			putchar(ch)
+			putchar(ch);
 		}
-		ch++
+		ch++;
 	}
 	putchar('\n');
 	return (0);
