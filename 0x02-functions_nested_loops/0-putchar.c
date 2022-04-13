21a1ca761d26137ff49ff63968e1632ea2a80d91
KwachOjunga <ojungakwach@gmail.com>
2022-04-13 05:15:25 +0300

    File that prints out '_putchar'

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ *main- prints the word _putchar
+ *
+ *return 0
+ */
+
+int main()
+{
+    printf ('_putchar');
+    putchar('\n');
+    return 0;
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,2 @@
+|File | Function|
+|0_putchar.c | prints out put_char|
